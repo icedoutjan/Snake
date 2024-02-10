2732f83682e025c862ea9878fef451cb72c32c33
icedoutjan <120628427+icedoutjan@users.noreply.github.com>
2024-02-10 13:13:06 +0100

    Create snake.cpp
    I created this file to improve the readability of the code. it's just the same code but in a separate file.

---
+++ snake.cpp
@@ -0,0 +1,66 @@
+#include "Arduino.h"
+#include <Keypad.h>
+#include "snake.h"
+
+//keypad or input stuff
+const byte ROWS = 4; // Anzahl der Zeilen des Keypads
+const byte COLS = 4; // Anzahl der Spalten des Keypads
+
+// Definition der Zeilen- und Spaltenpins des Keypads
+byte rowPins[ROWS] = { 53, 52, 51, 50 }; // Verbinde die Zeilen des Keypads mit diesen Pins
+byte colPins[COLS] = { 49, 48, 47, 46 }; // Verbinde die Spalten des Keypads mit diesen Pins
+
+// Definition der Tastatur
+const char keys[ROWS][COLS] = {
+  {'1','2','3','A'},
+  {'4','5','6','B'},
+  {'7','8','9','C'},
+  {'*','0','#','D'}
+};
+
+Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
+
+
+void snake::move(){
+  for (int i = len - 1; i > 0; i--) { 
+    body_x[i] = body_x[i - 1];
+    body_y[i] = body_y[i - 1];
+  }
+  switch (direction) {
+    case 0: // Links
+      body_x[0] -= 1;
+      break;
+    case 1: // Rechts
+      body_x[0] += 1;
+      break;
+    case 2: // Oben
+      body_y[0] -= 1;
+      break;
+    case 3: // Unten
+      body_y[0] += 1;
+      break;
+  }
+}
+
+bool snake::alife(){
+  if(body_y[0] == 8 | body_y[0] == -1 | body_x[0] == 8 | body_x[0] == -1){
+    return false;
+  }
+  for(int i = 4;i < len;i++){ // the loop starts at 4 becaus its impossible that the snake hits them self in this parts 
+    if(body_y[0] == body_y[i] && body_x[0] == body_x[i]){
+      return false;
+    }
+  }
+  return true;
+}
+
+
+void snake::updateDirection(){
+  char key = keypad.getKey();
+  if (key) {
+    if (key == '8' && direction != 3) direction = 2; // Nach oben
+    else if (key == '2' && direction != 2) direction = 3; // Nach unten
+    else if (key == '4' && direction != 1) direction = 0; // Nach links
+    else if (key == '6' && direction != 0) direction = 1; // Nach rechts
+  }
+}
