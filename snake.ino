2a9a04896fa08d24571494a0293715611ab429a3
icedoutjan <120628427+icedoutjan@users.noreply.github.com>
2024-02-11 19:52:13 +0100

    Update snake.ino
    change of some comments and the oder of commands in the main loop

--- snake.ino
+++ snake.ino
@@ -1,21 +1,21 @@
-#include <LedControl.h>
+ #include <LedControl.h>
 #include "snake.h"
 
-class appel{
+class apple{
   public:
   void spwanNew();
   void bling();
-  int appel_x;
-  int appel_y;
+  int apple_x;
+  int apple_y;
 
   private:
-  const float appelInterval = 200.0;  //to controll the speed of the blinking appel
-  float lastAppelTime = 0.0;
+  const float appleInterval = 200.0;  //to controll the speed of the blinking appel
+  float lastAppleTime = 0.0;
 };
 
 //init of some importent classes
 LedControl lc = LedControl(2, 3, 4, 1);
-appel appel;
+apple apple;
 snake snake;
 
 //to controll die fps of the main game
@@ -32,9 +32,9 @@ void setup() {
   Serial.begin(9600);
   lc.shutdown(0, false);
   lc.setIntensity(0, 1);
-  
   lc.clearDisplay(0);
-  appel.spwanNew();
+
+  apple.spwanNew();
 }
 
 void loop() {
@@ -48,9 +48,10 @@ void loop() {
       frameInterval = 1000.0 / intervalSpeed; //for speedup reasons
 
       //game logic
-      snake.move(); //edit the array that i use to print the snake based on the direction
+      snake.move(); 
+      //check after moving the snake does the snake hit a forbind point 
       gameOver = !snake.alife(); // for performanc reasons 
-      eat(); //does the snake eat teh appel and if yes speedup a bit and add a part to the snaek
+      eat(); 
 
       //visual stuff
       lc.clearDisplay(0);   //clear the led martix
@@ -59,7 +60,7 @@ void loop() {
       }
     }
     //also for visualisation but it have to be out side of the if() because the had another time logic 
-    appel.bling();
+    apple.bling();
     //input stuff
     snake.updateDirection();
   } 
@@ -79,25 +80,25 @@ bool feldInUse(int x,int y){  // prüf ob das feld von der schlange belegt ist
   return false;
 }
 
-void eat(){
-  if(snake.body_x[0] == appel.appel_x && snake.body_y[0] == appel.appel_y){
+void eat(){ //does the snake eat the appel and if yes speedup a bit and add a part to the snaek
+  if(snake.body_x[0] == apple.apple_x && snake.body_y[0] == apple.apple_y){
     snake.len++;
-    appel.spwanNew();
+    apple.spwanNew();
   }
 }
 
 // appel funktionen
-void appel::spwanNew(){ //spwan a new appel and speedup the game a bit 
-  while(feldInUse(appel_x,appel_y)){
-    appel_x = random(0,8); //just random() funktion is useless without randomSeed()
-    appel_y = random(0,8);// i am not sure but i think the code is faster with randomSeed()
+void apple::spwanNew(){ //spwan a new appel and speedup the game a bit 
+  while(feldInUse(apple_x,apple_y)){
+    apple_x = random(0,8); //just random() funktion is useless without randomSeed()
+    apple_y = random(0,8);// i am not sure but i think the code is faster with randomSeed()
   }
   intervalSpeed += 0.1;
 }
 
-void appel::bling(){ //just a simpel way to let the led blinging wihtout delay
-  if (currentTime - lastAppelTime >= appelInterval) {
-    lc.setLed(0,appel_x,appel_y,true);
-    lastAppelTime = currentTime;
+void apple::bling(){ //just a simpel way to let the led blinging wihtout delay
+  if (currentTime - lastAppleTime >= appleInterval) {
+    lc.setLed(0,apple_x,apple_y,true);
+    lastAppleTime = currentTime;
   }
 }
