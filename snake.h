8daeb96c675de9770be14dea600a7170c6ee76d9
icedoutjan <120628427+icedoutjan@users.noreply.github.com>
2024-02-16 17:43:13 +0100

    Update snake.h

--- snake.h
+++ snake.h
@@ -4,6 +4,7 @@
 
 class snake{
   public:
+  
   int len = 3;
   int direction = 3; // Anfangsrichtung: 0 = links, 1 = rechts, 2 = oben, 3 = unten
   int body_x[32];
@@ -11,6 +12,15 @@ class snake{
   void move();
   bool alife();
   void updateDirection();
+  void setup();
+  void updateDirectionJoystick();
+  //void dead();
+  void reset();
+  private:
+  int lastDirection;
+  const int maxlen = 32;
+  int xValue;
+  int yValue;
 };
 
 #endif
